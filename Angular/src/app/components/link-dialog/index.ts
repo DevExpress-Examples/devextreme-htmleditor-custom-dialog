3feaebf683f9fa62b4b79539c4980bec308d2770
LeSe0-devexpress <levon.stepanyan@devexpress.com>
2026-05-12 21:30:12 +0400

    feat: add link dialog component with HTML editor and URL validation

---
+++ Angular/src/app/components/link-dialog/index.ts
@@ -0,0 +1,2 @@
+export { LinkDialogComponent } from './link-dialog.component';
+export { LinkDialogData } from './link-dialog.types';
---
+++ Angular/src/app/components/link-dialog/link-dialog.types.ts
@@ -0,0 +1,7 @@
+export interface LinkDialogData {
+  index: number;
+  length: number;
+  selectedText: string;
+  selectedFormats: Record<string, unknown>;
+  url: string;
+}
